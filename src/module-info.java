d3daaaaf591cf7a1918d2866809b6fbada61d129
Heer Patel <83523957+heer-jp@users.noreply.github.com>
2021-11-01 11:58:17 -0400

    Add files via upload

--- src/module-info.java
+++ src/module-info.java
@@ -1,1 +1,12 @@
+module Exampl {
+    exports unittesting;
+    exports tuition;
 
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires junit;
+    requires javafx.controls;
+    
+    opens tuition to javafx.fxml;
+}
